90d72291ce0bb28cd7a13a8429d4488ccce756f2
Matt Batman <mbatman2@ivytech.edu>
2017-01-27 00:20:03 -0500

    Implemented rxjs example

--- js/app.js
+++ js/app.js
@@ -1,3 +1,66 @@
+import { Observable } from 'rxjs/Observable';
 import './rxjs-operators';
 
-console.log('hello world');
+// DOM references
+let button = document.querySelector('button');
+let body = document.querySelector('body');
+
+// state
+let lightsOn = true;
+
+// Observable
+// http://stackoverflow.com/questions/34533158/create-a-toggle-button-with-rxjs
+let button$ = Observable.fromEvent(button, 'click')
+  .scan ((state, event) => {
+    return !state;
+  }, lightsOn);
+
+let on$ = button$
+  .filter(x => x)
+  .mapTo(body);
+
+let out$ = button$
+  .filter(x => !x)
+  .mapTo(body);
+
+// Observers
+let lightsOnObs = {
+  next: (x) => {
+    addClass(x, 'lights-on');
+    removeClass(x, 'lights-out');
+  },
+  error: (err) => { console.error(err); },
+  complete: () => { console.log('stream complete'); }
+};
+
+let lightsOutObs = {
+  next: (x) => {
+    addClass(x, 'lights-out');
+    removeClass(x, 'lights-on');
+  },
+  error: (err) => { console.error(err); },
+  complete: () => { console.log('stream complete'); }
+};
+
+// Subscription
+let onSub = on$.subscribe(lightsOnObs);
+let outSub = out$.subscribe(lightsOutObs);
+
+// helper functions
+// http://youmightnotneedjquery.com/
+function addClass(el, className) {
+  if (el.classList) {
+    el.classList.add(className);
+  } else {
+    el.className += ' ' + className;
+  }
+}
+
+function removeClass(el, className) {
+  if (el.classList) {
+    el.classList.remove(className);
+  } else {
+    el.className = el.className.replace(new RegExp('(^|\\b)' +
+    className.split(' ').join('|') + '(\\b|$)', 'gi'), ' ');
+  }
+}
--- js/rxjs-operators.js
+++ js/rxjs-operators.js
@@ -1,1 +1,4 @@
 import 'rxjs/add/observable/fromEvent';
+import 'rxjs/add/operator/filter';
+import 'rxjs/add/operator/scan';
+import 'rxjs/add/operator/mapTo';
